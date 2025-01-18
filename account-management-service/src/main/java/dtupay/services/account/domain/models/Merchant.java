dd434fe6d95746a4218585d89b24a89ce9a8fbc6
Zedrichu <adrzvizdencojr@gmail.com>
2025-01-18 12:49:36 +0100

    Merchant Record forgot

---
+++ account-management-service/src/main/java/dtupay/services/account/domain/models/Merchant.java
@@ -0,0 +1,28 @@
+package dtupay.services.account.domain.models;
+
+import jakarta.json.bind.annotation.JsonbCreator;
+import jakarta.json.bind.annotation.JsonbProperty;
+
+public record Merchant(String firstName, String lastName, String cpr, String bankAccountNo, String payId) {
+
+	/**
+	 * Main constructor of Merchant entity objects with JSON-B serialization tags for "value" properties
+	 * @param firstName
+	 * @param lastName
+	 * @param cpr
+	 * @param bankAccountNo
+	 * @param payId
+	 */
+	@JsonbCreator
+	public Merchant(@JsonbProperty String firstName,
+	                @JsonbProperty String lastName,
+	                @JsonbProperty String cpr,
+	                @JsonbProperty String bankAccountNo,
+	                @JsonbProperty String payId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.cpr = cpr;
+		this.bankAccountNo = bankAccountNo;
+		this.payId = payId;
+	}
+}
