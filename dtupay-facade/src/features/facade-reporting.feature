a0cdba5f85e4c5c8fe917173574b90d5d549fb32
Jonas Kjeldsen <jonas.lykke.kjeldsen@gmail.com>
2025-01-23 21:43:11 +0100

    Create additional Test in feature file and facadeReportStepDefs
    Co-authored-by: Simon Hermansen <simonhermansen5dk@gmail.com>
    Co-authored-by: Adrian Zvizdenco <adrzvizdencojr@gmail.com>

--- dtupay-facade/src/features/facade-reporting.feature
+++ dtupay-facade/src/features/facade-reporting.feature
@@ -4,23 +4,20 @@ Feature: dtupay.services.facade DTUPay Facade Reporting Feature
     When a customer is requesting a customer report with a non existing customer id
     Then the "CustomerReportRequested" event is sent with an non existing customer id
     When the "CustomerReportGenerated" event is received for customer
-    Then the retrieve report is empty
+    Then the retrieved customer report is empty
+
+  Scenario: Successful Merchant Report Request
+    Given a merchant that has performed no payments
+    When a merchant is requesting a merchant report with a non existing customer id
+    Then the "MerchantReportRequested" event is sent with an non existing merchant id
+    When the "MerchantReportGenerated" event is received for merchant
+    Then the retrieved merchant report is empty
 
 
-
-#  Scenario: Successful Merchant Report Request
-#    Given a registered merchant and report of merchant
-#    When a merchant report is requested with id
-#    Then the "MerchantReportRequest" event for merchant is sent with their id
-#    When the "MerchantReportGenerated" is received for the merchant id
-#    Then a merchant report is retrieved
-
-
-#  Scenario: Successful Manager Report Request
-#    Given a registered manager and report of manager
-#    When a manager report is requested with id
-#    Then the "MerchantReportRequest" event for manager is sent with their id
-#    When the "MerchantReportGenerated" is received for the manager id
-#    Then a manager report is retrieved
+  Scenario: Successful Manager Report Request
+    When a manager is requesting a manager report
+    Then the "ManagerReportRequested" event is sent
+    When the "ManagerReportGenerated" event is received for manager
+    Then the retrieved manager report is empty
 
 
--- dtupay-facade/src/test/java/tests/service/behaviours/FacadeReportStepDefs.java
+++ dtupay-facade/src/test/java/tests/service/behaviours/FacadeReportStepDefs.java
@@ -1,9 +1,10 @@
 package tests.service.behaviours;
 
-import dtupay.services.facade.domain.CustomerService;
 import dtupay.services.facade.domain.ReportService;
 import dtupay.services.facade.domain.models.*;
 import dtupay.services.facade.domain.models.views.CustomerView;
+import dtupay.services.facade.domain.models.views.ManagerView;
+import dtupay.services.facade.domain.models.views.MerchantView;
 import dtupay.services.facade.utilities.Correlator;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static org.junit.Assert.*;
@@ -22,20 +24,36 @@ import static org.junit.Assert.*;
 public class FacadeReportStepDefs {
 
     private Map<String, CompletableFuture<Event>> publishedEvents = new ConcurrentHashMap<>();
+    private CompletableFuture<Event> publishedEvent = new CompletableFuture<>();
 
-
-    private final Function<Event, String> tokenKeyExtractor =
+    private final Function<Event, String> IdKeyExtractor =
             (event) -> event.getArgument(0, String.class);
 
 
-    private final MessageQueue reportCustomerQ = new MockMessageQueue(tokenKeyExtractor, publishedEvents);
+    private final MessageQueue reportQ = new MockMessageQueue(IdKeyExtractor, publishedEvents);
+    private final MessageQueue managerQ = new MessageQueue() {
+        @Override
+        public void publish(Event event) {
+            publishedEvent.complete(event);
+        }
 
-    private ReportService reportService = new ReportService(reportCustomerQ);
+        @Override
+        public void addHandler(String topic, Consumer<Event> handler) {}
+    };
+
+    private ReportService reportService = new ReportService(reportQ);
+    private ReportService managerReportService = new ReportService(managerQ);
     private CompletableFuture<Report<CustomerView>> futureCustomerReport = new CompletableFuture<>();
+    private CompletableFuture<Report<MerchantView>> futureMerchantReport = new CompletableFuture<>();
+    private CompletableFuture<Report<ManagerView>> futureManagerReport = new CompletableFuture<>();
 
-    private Map<String, Correlator> cCorrelators = new ConcurrentHashMap<>();
+    private Map<String, Correlator> correlators = new ConcurrentHashMap<>();
+    private Correlator managerCorrelator;
 
     private String customerId;
+    private String merchantId;
+    private String managerId;
+
     @Given("a customer that has performed no payments")
     public void aCustomerThatHasPerformedNoPayments() {
         customerId = "cId";
@@ -62,20 +80,97 @@ public class FacadeReportStepDefs {
 
         var eCustomerId = event.getArgument(0,String.class);
         var correlator = event.getArgument(1,Correlator.class);
-        cCorrelators.put(eCustomerId, correlator);
+        correlators.put(eCustomerId, correlator);
     }
 
     @Then("the {string} event is received for customer")
     public void aEventIsReceivedWithAnEmptyReport(String eventType) {
-        var correlator = cCorrelators.get(customerId);
+        var correlator = correlators.get(customerId);
         Report<CustomerView> customerRep = new Report<>(new ArrayList<CustomerView>());
         assertNotNull(correlator);
         reportService.handleCustomerReportGenerated(new Event(eventType,customerRep,correlator));
     }
 
-    @Then("the retrieve report is empty")
-    public void theRetrieveReportIsEmpty() {
+    @Then("the retrieved customer report is empty")
+    public void theRetrievedCustomerReportIsEmpty() {
         Report<CustomerView> customerRep = futureCustomerReport.join();
         assertEquals(0, customerRep.getEntries().size());
+    }
+
+    @Given("a merchant that has performed no payments")
+    public void aMerchantThatHasPerformedNoPayments() {
+        merchantId = "mId";
+        publishedEvents.put(merchantId, new CompletableFuture<>());
+    }
+
+    @When("a merchant is requesting a merchant report with a non existing customer id")
+    public void aMerchantIsRequestingAMerchantReportWithANonExistingCustomerId() {
+        new Thread(() -> {
+            try {
+                var result = reportService.getMerchantReport(merchantId);
+                futureMerchantReport.complete(result);
+            } catch (Exception e) {
+                futureMerchantReport.completeExceptionally(e);
+            }
+        }).start();
+    }
+
+    @Then("the {string} event is sent with an non existing merchant id")
+    public void theEventIsSentWithAnNonExistingMerchantId(String eventType) {
+        Event event = publishedEvents.get(merchantId).join();
+        assertEquals(eventType, event.getTopic());
+
+        var eMerchantId = event.getArgument(0,String.class);
+        var correlator = event.getArgument(1,Correlator.class);
+        correlators.put(eMerchantId, correlator);
+    }
+
+    @When("the {string} event is received for merchant")
+    public void theEventIsReceivedForMerchant(String eventType) {
+        var correlator = correlators.get(merchantId);
+        Report<MerchantView> merchantRep = new Report<>(new ArrayList<MerchantView>());
+        assertNotNull(correlator);
+        reportService.handleMerchantReportGenerated(new Event(eventType,merchantRep,correlator));
+    }
+
+    @Then("the retrieved merchant report is empty")
+    public void theRetrievedMerchantReportIsEmpty() {
+        Report<MerchantView> merchantRep = futureMerchantReport.join();
+        assertEquals(0, merchantRep.getEntries().size());
+    }
+
+    @When("a manager is requesting a manager report")
+    public void aManagerIsRequestingAManagerReport() {
+        new Thread(() -> {
+            try {
+                var result = managerReportService.getManagerReport();
+                futureManagerReport.complete(result);
+            } catch (Exception e) {
+                futureManagerReport.completeExceptionally(e);
+            }
+        }).start();
+    }
+
+    @Then("the {string} event is sent")
+    public void theEventIsSent(String eventType) {
+        Event event = publishedEvent.join();
+        assertEquals(eventType, event.getTopic());
+
+        var correlator = event.getArgument(0,Correlator.class);
+        managerCorrelator = correlator;
+    }
+
+    @When("the {string} event is received for manager")
+    public void theEventIsReceivedForManager(String eventType) {
+        var correlator = managerCorrelator;
+        Report<ManagerView> managerRep = new Report<>(new ArrayList<ManagerView>());
+        assertNotNull(correlator);
+        managerReportService.handleManagerReportGenerated(new Event(eventType,managerRep,correlator));
+    }
+
+    @Then("the retrieved manager report is empty")
+    public void theRetrievedManagerReportIsEmpty() {
+        Report<ManagerView> managerRep = futureManagerReport.join();
+        assertEquals(0, managerRep.getEntries().size());
     }
 }
