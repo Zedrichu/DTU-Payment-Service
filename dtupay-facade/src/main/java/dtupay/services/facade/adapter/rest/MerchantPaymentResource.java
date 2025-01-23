42d42a39b7394e47042de259b095f85545ea0812
JepMik <jepmik@live.dk>
2025-01-23 22:05:59 +0100

    Swagger documentation
    Co-authored-by: scones111 <jonas.lykke.kjeldsen@gmail.com>
    Co-authored-by: Adrian Zvizdenco <adrzvizdencojr@gmail.com>
    Co-authored-by: simonhermansen <simonhermansen5dk@gmail.com>
    Co-authored-by: Ursu Adrian <uursu.adrian@gmail.com>

--- dtupay-facade/src/main/java/dtupay/services/facade/adapter/rest/MerchantPaymentResource.java
+++ dtupay-facade/src/main/java/dtupay/services/facade/adapter/rest/MerchantPaymentResource.java
@@ -21,6 +21,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletionException;
 
 @Path("/merchants/{mid}/payments")
 public class MerchantPaymentResource {
@@ -33,12 +34,13 @@ public class MerchantPaymentResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response requestTokens(PaymentRequest paymentRequest) {
         logger.info("Merchant payment request received: {}", paymentRequest);
-        boolean paymentSuccess = merchantService.pay(paymentRequest);
-        if (paymentSuccess) {
+        try {
+            var paymentSuccess = merchantService.pay(paymentRequest);
             return Response.ok().build();
+        } catch (CompletionException e) {
+            String error = "Payment failed " + e.getCause().getMessage();
+            return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
         }
-        return Response.serverError().build();
-
     }
 
 }
